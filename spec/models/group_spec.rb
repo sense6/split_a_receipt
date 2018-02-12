3e42433d89e7e983f88bbe49170fa01069af0703
sense6 <konrad.worobiej@gmail.com>
2018-02-12 16:31:22 +0100

    User membership group association specs

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  describe "associations" do
+    it { should have_many(:users).through(:memberships) }
+    it { should have_many(:memberships) }
+  end
+end
