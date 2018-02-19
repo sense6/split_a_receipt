d8d48016045ff0c90cb9e553b8ae1bd0c66236e7
sense6 <konrad.worobiej@gmail.com>
2018-02-19 18:32:27 +0100

    Fix validation bug

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,13 +1,13 @@
 class Membership < ApplicationRecord
   enum member_type: { member: 0, admin: 1 }
-  validate :user_cant_join_same_group_twice
+  validate :user_cant_join_same_group_twice, on: :create
 
   belongs_to :user
   belongs_to :group
 
   def user_cant_join_same_group_twice
     if Membership.where(user_id: user_id, group_id: group_id).exists?
-      errors.add(:group_id, "cant join same group twice")
+      errors.add(:user_id, "cant join same group twice")
     end
   end
 end
