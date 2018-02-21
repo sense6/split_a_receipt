e06e59c0a1cd31bd977c9dff788d0d4001545567
sense6 <konrad.worobiej@gmail.com>
2018-02-21 15:03:22 +0100

    Group model specs

---
+++ spec/factories/groups.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :group do
+    name "group"
+    description "desc"
+  end
+end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Group, type: :model do
     it { should have_many(:memberships) }
     it { should have_many(:invitations) }
   end
+
+  user = FactoryBot.create(:user)
+  user2 = FactoryBot.create(:user)
+  group = FactoryBot.create(:group)
+  membership = FactoryBot.create(:membership, user_id: user.id, group_id: group.id)
+
+  describe "member?" do
+    it "checks if user is a member of a group" do
+      expect(group.member?(user)).to be true
+    end
+    it "checks if user is not a member of a group" do
+      expect(group.member?(user2)).to be false
+    end
+  end
+  describe "has_admin? and set_admin" do
+    it "checks if group doesn't have an admin" do
+      expect(group.has_admin?).to be false
+    end
+    it "checks if group has an admin" do
+      group.set_admin
+      expect(group.has_admin?).to be true
+    end
+  end
+
 end
