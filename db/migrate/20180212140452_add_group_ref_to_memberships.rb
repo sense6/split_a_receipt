4a0a31e6b3766ef341f047458ba1143e765702f5
sense6 <konrad.worobiej@gmail.com>
2018-02-25 21:53:05 +0100

    Fix mysql error

--- db/migrate/20180212140452_add_group_ref_to_memberships.rb
+++ db/migrate/20180212140452_add_group_ref_to_memberships.rb
@@ -1,5 +1,5 @@
 class AddGroupRefToMemberships < ActiveRecord::Migration[5.1]
   def change
-    add_reference :memberships, :group, foreign_key: {on_delete: :cascade}
+    add_reference :memberships, :group
   end
 end
