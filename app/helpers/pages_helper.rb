7216b2aa4bf084b6a83b448bdcda2047b07db767
sense6 <konrad.worobiej@gmail.com>
2018-02-25 21:52:38 +0100

    Refactor pages views

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def badge_type(group)
+    current_user.admin?(group) ? badge("Admin") : badge("Member")
+  end
+
+  def badge(content)
+    "<span class='badge badge-pill badge-primary'>#{content}</span>".html_safe
+  end
 end
